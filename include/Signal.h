1209a9168e1eda884256f67336861141fe84a0c1
Feyzan Aydemir <feyzan.aydemir@gmail.com>
2024-08-05 21:50:17 +0300

    Add Signal.h

---
+++ include/Signal.h
@@ -0,0 +1,49 @@
+#ifndef SIGNAL_H
+#define SIGNAL_H
+
+////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////
+#include <QLineEdit>
+#include <QLineSeries>
+#include <QMap>
+#include <QString>
+#include <functional>
+#include "signalGeneration.h"
+#include "windowing.h"
+#include "MainWindow.h"
+
+QT_CHARTS_USE_NAMESPACE
+
+
+////////////////////////////////////////////////////////
+// Class Definition
+////////////////////////////////////////////////////////
+class Signal {
+    public:
+        Signal(const QString &name, const QStringList &params) {
+            for (const QString& param : params) {
+                parameters[param] = new QLineEdit();
+            }
+
+            signalType = name;
+        }
+
+        double operator[](const QString& key) const {
+            return parameters.value(key, nullptr)->text().toDouble();
+        }
+
+        QString signalType;
+        QString windowType;
+        QLineSeries *series = new QLineSeries;
+        QLineSeries *fftSeries;
+        QLineSeries *windowedSeries;
+        QLineSeries *windowedFFTSeries;
+        QMap<QString, QLineEdit*> parameters;
+        void generate(void);
+
+    private:
+        QLineSeries *computeFFT(QLineSeries*);
+};
+
+#endif
